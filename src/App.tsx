eca2f58aab526538111496246ed7619006cda274
TerjeBallestad <terj.ba@gmail.com>
2026-01-22 16:20:39 +0100

    feat(05-03): integrate TalentSelectionModal into App
    - Import and render TalentSelectionModal
    - Modal auto-shows when talent offer available

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { ActivityPanel } from './components/ActivityPanel';
 import { CharacterPanel } from './components/CharacterPanel';
 import { SimulationControls } from './components/SimulationControls';
 import { SkillTreePanel } from './components/SkillTreePanel';
+import { TalentSelectionModal } from './components/TalentSelectionModal';
 import { useCharacterStore } from './stores/RootStore';
 
 const App = observer(function App() {
@@ -17,6 +18,7 @@ const App = observer(function App() {
   return (
     <div className="flex min-h-screen bg-base-100">
       <Toaster position="bottom-right" richColors />
+      <TalentSelectionModal />
       {/* Sidebar */}
       <CharacterPanel />
 
